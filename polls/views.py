12f29059c3328024e175ed919aefd0ff770aa3cc
Maria Cristina Fadgyas <fadgyas.maria@yahoo.com>
2026-03-11 17:24:18 +0100

    refactor index view to use render shortcut

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
 from .models import Question
@@ -6,9 +7,10 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {"latest_question_list": latest_question_list}
-    return HttpResponse(template.render(context, request))
+    # template = loader.get_template("polls/index.html")
+    # return HttpResponse(template.render(context, request))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request, question_id):
